156e812ba87ca27517442d34d8442488eb4fe879
daveKim <48761100+salme4@users.noreply.github.com>
2020-08-26 14:43:40 +0900

    Update main.tf

--- terraform/dave-terraform/main.tf
+++ terraform/dave-terraform/main.tf
@@ -1,15 +1,10 @@
 provider "aws" {
     region = "ap-northeast-1"
-    shared_credentials_file = "~/.aws/credentials"
 }
 
-# resource "aws_ec2" "daveEc2" {
-#     config options..
-#     key = "value"
-# }
-sg-0c2432adb510bec67
+
 resource "aws_instance" "dave-test" {
-    ami = "ami-0cc75a8978fbbc969"
+    ami = "ami-0edf7f6a9013cfb72"
     instance_type = "t2.micro"
     subnet_id = "subnet-026cbdf685ee58384"
 }
